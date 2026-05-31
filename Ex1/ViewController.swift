241ee036620898d1a9bc57ddd389cbd7e071b23b
Nir Epelbaum <nirepel@gmail.com>
2026-05-31 10:35:53 +0300

    Initial Commit

---
+++ Ex1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ex1
+//
+//  Created by Nir Epelbaum on 31/05/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
